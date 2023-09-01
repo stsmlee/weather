aaf1543e9a281c79a6cea0f97f4c77e383da510a
Steph Lee <stsmlee@gmail.com>
2023-09-01 16:39:12 -0400

    did some styling

--- src/api.js
+++ src/api.js
@@ -1,3 +1,17 @@
-export default function sayHi() {
-  console.log('HEY!');
+export default async function callAPI(location) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=2000ac3edfaa40c689f152811232808&q=${location}&days=3&aqi=yes&alerts=yes`,
+      { mode: 'cors' },
+    );
+    const responseJSON = await response.json();
+    if (response.status !== 200) {
+      console.log('oops: ', response.status, responseJSON.error.message);
+    } else {
+      console.log(response.headers);
+      console.log(responseJSON);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import './style.css';
-import sayHi from './api';
+import callAPI from './api';
 
-const str = 'string';
-console.log(str);
-const str2 = "It's whatever";
-console.log(str2);
-const str3 = "It's fine";
-console.log(str3);
-
-sayHi();
+callAPI('New York');
