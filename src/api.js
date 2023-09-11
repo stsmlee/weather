655aa9556d2915a827961be79a4bcf067b332f03
Steph Lee <stsmlee@gmail.com>
2023-09-11 11:22:29 -0400

    combined forecast 1 and 2 into one function

--- src/api.js
+++ src/api.js
@@ -22,6 +22,7 @@ const weekdays = [
   'Friday',
   'Saturday',
 ];
+const weekdaysShort = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 let data;
 const unitSwitch = document.getElementById('unit-switch');
 const locationDiv = document.getElementById('location-div');
@@ -66,23 +67,48 @@ const todaySnow = document.getElementById('today-chance-of-snow');
 const todayHumidity = document.getElementById('today-humidity');
 const next24Hours = document.getElementById('next-24-hours-div');
 
+const day1date = document.getElementById('day-1-date');
+const day1LoHi = document.getElementById('day-1-lo-hi');
+const day2date = document.getElementById('day-2-date');
+const day2LoHi = document.getElementById('day-2-lo-hi');
+
 function updateUnits() {
   if (unitSwitch.checked) {
     currentTemp.textContent = `Current Temp: ${data.current.temp_c}°C, but feels like: ${data.current.feelslike_c}°C`;
     currentWind.textContent = `Wind/Gust/Direction: ${data.current.wind_kph}kph / ${data.current.gust_kph}kph / ${data.current.wind_dir}`;
     visibility.textContent = `Visibility: ${data.current.vis_km}km`;
     todayLoHi.textContent = `Lo: ${data.forecast.forecastday[0].day.mintemp_c}°C / Hi: ${data.forecast.forecastday[0].day.maxtemp_c}°C`;
+    day1LoHi.textContent = `Lo: ${data.forecast.forecastday[1].day.mintemp_c}°C / Hi: ${data.forecast.forecastday[1].day.maxtemp_c}°C`;
+    day2LoHi.textContent = `Lo: ${data.forecast.forecastday[2].day.mintemp_c}°C / Hi: ${data.forecast.forecastday[2].day.maxtemp_c}°C`;
   } else {
     currentTemp.textContent = `Current Temp: ${data.current.temp_f}°F, but feels like: ${data.current.feelslike_f}°F`;
     currentWind.textContent = `Wind/Gust/Direction: ${data.current.wind_mph}mph / ${data.current.gust_mph}mph / ${data.current.wind_dir}`;
     visibility.textContent = `Visibility: ${data.current.vis_miles}mi`;
     todayLoHi.textContent = `Lo: ${data.forecast.forecastday[0].day.mintemp_f}°F / Hi: ${data.forecast.forecastday[0].day.maxtemp_f}°F`;
+    day1LoHi.textContent = `Lo: ${data.forecast.forecastday[1].day.mintemp_f}°F / Hi: ${data.forecast.forecastday[1].day.maxtemp_f}°F`;
+    day2LoHi.textContent = `Lo: ${data.forecast.forecastday[2].day.mintemp_f}°F / Hi: ${data.forecast.forecastday[2].day.maxtemp_f}°F`;
   }
 }
 
 unitSwitch.addEventListener('click', () => {
   if (data) updateUnits();
 });
+
+function updateForecast(idx) {
+  const condition = document.getElementById(`day-${idx}-condition`);
+  const precip = document.getElementById(`day-${idx}-chance-of-precip`);
+  const icon = document.getElementById(`day-${idx}-icon`);
+  condition.textContent = data.forecast.forecastday[idx].day.condition.text;
+  icon.src = `../weather_icons/day/${data.forecast.forecastday[
+    idx
+  ].day.condition.icon.slice(-7)}`;
+  const rainChance = data.forecast.forecastday[idx].day.daily_chance_of_rain;
+  const snowChance = data.forecast.forecastday[idx].day.daily_chance_of_snow;
+  let percent;
+  if (rainChance > snowChance) percent = rainChance;
+  else percent = snowChance;
+  if (percent > 0) precip.textContent = `${percent}%`;
+}
 
 function updateDisplay() {
   const currentEpoch = data.current.last_updated_epoch;
@@ -127,8 +153,23 @@ function updateDisplay() {
   if (todayRainChance > 0)
     todayRain.textContent = `Chance of rain: ${todayRainChance}%`;
   if (todaySnowChance > 0)
-    todaySnow.textContent = `Chance of rain: ${todaySnowChance}%`;
+    todaySnow.textContent = `Chance of snow: ${todaySnowChance}%`;
+
   next24Hours.replaceChildren();
+
+  if (currentDate.getDay() + 1 < 7) {
+    day1date.textContent = weekdaysShort[currentDate.getDay() + 1];
+  } else day1date.textContent = weekdaysShort[0];
+  updateForecast(1);
+
+  const day2day = currentDate.getDay() + 2;
+  if (day2day < 7) {
+    day2date.textContent = weekdaysShort[day2day];
+  } else if (day2day === 7) {
+    day2date.textContent = weekdaysShort[0];
+  } else day2date.textContent = weekdaysShort[1];
+  updateForecast(2);
+
   updateUnits();
 }
 
